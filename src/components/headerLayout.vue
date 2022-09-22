d31e201cddc9b0c24d0857499aa6b1603d880714
chris <anyikamaduchris@gmail.com>
2022-09-21 23:59:06 -1100

    New

---
+++ src/components/headerLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <Header>
+        <h1>{{ title }}</h1>
+    </Header>
+</template>
+
+<script>
+
+
+
+
+export default {
+   name:"headerLayout",
+   props: {
+    title : {
+        type : String,
+    }
+   },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
