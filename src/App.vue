f9eb27bcfa2c0e8b0474e3a670904569acbe73ca
chris <anyikamaduchris@gmail.com>
2022-10-02 21:24:28 -1100

    commit

--- src/App.vue
+++ src/App.vue
@@ -45,12 +45,12 @@ body {
   margin: auto;
 }
 .container {
-  max-width: 500px;
+  max-width: auto;
   overflow: auto;
   min-height: 300px;
   border: 1px solid steelblue;
   margin: auto;
-  margin-top: 50px;
+  margin: 50px 0px 50px 0px;
   padding: 30px;
   border-radius: 5px;
 }
